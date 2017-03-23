9faae244a2339f9a646ec0c35fa112098c02bdeb
Andy Fang <andy@mingtak.com.tw>
2017-03-23 12:35:13 +0000

    update

---
+++ src/jitop/theme/browser/views.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone import api
+
+
+class JitopMacro(BrowserView):
+
+    def __call__(self):
+        """  """
