12b0b24ddf62fe50ddaa014d7e3e28504fd55db9
Andy Fang <andy@mingtak.com.tw>
2017-03-24 02:25:25 +0000

    update 0324-1

--- src/jitop/theme/browser/views.py
+++ src/jitop/theme/browser/views.py
@@ -7,4 +7,12 @@ from plone import api
 class JitopMacro(BrowserView):
 
     def __call__(self):
-        """  """
+        folderName = ['about', 'service', 'showcase', 'partner', 'media']
+        self.results = {}
+
+        for folderItem in folderName:
+            self.results[folderItem] = []
+            brain = portal[folderItem].getChildNodes()
+            for item in brain:
+                if item.Type() == 'Page':
+                    self.results[folderItem].append(item)
